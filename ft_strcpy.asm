b4303e661e4345a0edef4c8060afe60bc283bbbf
kurz-m <m.kurz92@gmail.com>
2024-02-29 23:41:56 +0100

    change counter to rcx (counter register)

--- ft_strcpy.asm
+++ ft_strcpy.asm
@@ -13,17 +13,17 @@ section .text
 global FT_STRCPY
 
 FT_STRCPY:
-    xor rdx, rdx ; size_t i = 0
+    xor rcx, rcx ; size_t i = 0
     mov rax, rdi ; copy pointer for later return
     test sil, sil ; *(s) == '\0'
     jz .end_strcpy ; if '\0', jump to return statement
-    mov cl, BYTE [rsi + rdx] ; copy *(src + rdx) to lower byte of rcx
+    mov dl, BYTE [rsi + rcx] ; copy *(src + rcx) to lower byte of rcx
 
 .loop:
-    mov BYTE [rdi + rdx], cl ; copy char to *(dest + rdx)
-    inc rdx ; increase rdx by 1 (++rdx)
-    mov cl, BYTE [rsi + rdx] ; copy *(src + rdx) to lower byte of rcx
-    test cl, cl ; check of null-termination: char & char
+    mov BYTE [rdi + rcx], dl ; copy char to *(dest + rcx)
+    inc rcx ; increase rcx by 1 (++rcx)
+    mov dl, BYTE [rsi + rcx] ; copy *(src + rcx) to lower byte of rcx
+    test dl, dl ; check of null-termination: char & char
     jne .loop ; jump to loop if char is not equal '\0'
 
 .end_strcpy:
